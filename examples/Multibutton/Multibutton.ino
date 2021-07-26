9c1780b61a90af65290ada4550f25e659733096c
Sami Kaukasalo <79233780+sakabug@users.noreply.github.com>
2021-07-27 01:08:49 +0300

    Small adjusts

--- examples/Multibutton/Multibutton.ino
+++ examples/Multibutton/Multibutton.ino
@@ -9,8 +9,6 @@ const uint8_t buttonPins[buttonCount] = {2,3,4,5,6};
 Bugtton buttons(buttonCount, buttonPins, INPUT_PULLUP, 25);
 
 bool flipFlop = false;
-bool awesome = false;
-int8_t count1 = -1;
 
 void setup() {
     Serial.begin(57600);
@@ -20,7 +18,8 @@ void loop() {
     
     // Update button states handler
     buttons.update();
-
+    
+    // Testing buttons
     for (uint8_t i=0; i<buttonCount; i++){
         if (buttons.fell(i)) {
             Serial.print("B");
@@ -34,25 +33,26 @@ void loop() {
         }
     }
 
-    
+    // Printing continuous held time
     if (buttons.held(0)) {
         Serial.print("B0 held for ");
         Serial.print(buttons.duration(0));
         Serial.println(" ms");
     }
 
-    // If button 2 held for 1 sec - oneshot
+    // If button 2 held for 1 sec, destroy something?
     if (buttons.heldUntil(1,1000)) {
         flipFlop = !flipFlop;
         if (flipFlop) Serial.println("SELF DESTRUCT ACTIVATED");
         else Serial.println("SELF DESTRUCT DEACTIVATED");
     }
 
-    // If button 2 is left unpressed for 5 sec - it tells user to press it - oneshot
+    // If button 2 is left unpressed for 10 sec - it tells user to press it
     if (buttons.upUntil(2,10000)) {
         Serial.println("Try holding B2");
     }
-
+    
+    // Ticks once every 1000 ms
     if (buttons.intervalTick(2,1000)){
         Serial.print("B2 pressed for ");
         Serial.print(buttons.duration(2)/1000);
